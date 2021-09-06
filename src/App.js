a4a81e7cdce7486bfd5082065db9b1aedbf9a93b
kwame2 <softcons99@gmail.com>
2021-09-06 14:02:44 +0200

    feature 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-function App() {
-  return (
-    <div className="App">
-      <div className="grid-container">
-        <header>
-          <a href="/">React Shopping Cart</a>
-        </header>
-        <main>Product List</main>
-        <footer>All right is reserved.</footer>
+import React from "react"
+import Products from "./components/Products"
+import data from "./data.json"
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="grid-container">
+          <header>
+            <a href="/">React Shopping Cart</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                <Products products={this.state.products} />
+              </div>
+              <div className="sidebar">Cart Items</div>
+            </div>
+          </main>
+          <footer>All right is reserved</footer>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-export default App
